31674a015a775fc1bf3c4d2557be723bce8a681d
pubudu Hewavithana <64272083+pubuduprabhathiya@users.noreply.github.com>
2021-03-13 15:45:16 +0530

    Add files via upload

--- tests/intergration tests/channel/channel.test.js
+++ tests/intergration tests/channel/channel.test.js
@@ -1,165 +1,196 @@
-const channel = require("../../../controllers/channel");
-
+const channelController = require("../../../controllers/channel");
 const request = require("supertest");
 
-const {
-  getchannel,
-  gettimeslot,
-  getday,
-  scheduleChannel,
-  getTimeTable,
-  getprogramTable,
-  addfeedback,
-  getuserId,
-  addProgram,
-} = require("../../../models/userModel");
-
+let server;
 let pool;
 
-describe("/channel tests",  () => {
-  let res;
+describe("/Channel tests", () => {
+  const res = {
+    render: null,
+    redirect: null,
+  };
 
-  
   beforeEach(async () => {
-    res = {
-      render: null,
-      redirect: null,
-    };
-   
+    server = require("../../../index").server;
     pool = require("../../../config/database");
     res.render = jest.fn();
     res.redirect = jest.fn();
   });
   afterEach(async () => {
-    
     res.render = null;
     res.redirect = null;
+    server.close();
   });
 
   afterAll(async () => {
-    //await pool.end();
-    
+    await pool.end();
   });
 
-  //GET CHANNEL
-  describe("/channel get_channel",  () => {
-
-   
-    beforeEach(async () => {
-       
-        req = {
-            url : '/timetable',
+  describe("GET /timetable Timetable page view render test", () => {
+    it("Should return Timetable Page", async () => {
+        const req = {
             query: {},
-            session: {
-              user_id: null,
-              type: null,
+            params: {
+                id:"test1"
             },
-            params:{
-              id:"channel_id"
-            }
-          };
+        session: {
+          user_id: null,
+          type: null,
+        },
+      };
+        res.locals = {
+            channel: undefined,
+            ch:undefined,
+        };
+      
 
-          res ={
-            locals:{
-              channel:null,
-              ch:null
-            },
-            render:jest.fn()
-          };
-    
-      });
-
-    it("should return user/timetable and empty timetable", async () => {
-      
-      
-     
-      await channel.get_channel(req, res);
-      expect(res.render).toHaveBeenCalledWith(`user/timetable`);
-      expect(res.locals.channel.timetable).toEqual("");
-      
+      await channelController.get_channel(req, res);
+      expect(res.render).toHaveBeenCalledWith("user/timetable");
     });
-
-
-    it("should return user/timetable and timetable", async () => {
-      
-      req.url = "/nottimetable";
-      expected ={timetable:await getTimeTable(req.params.id)};
-
-     
-      await channel.get_channel(req, res);
-      expect(res.render).toHaveBeenCalledWith(`user/timetable`);
-      expect(res.locals.channel.timetable).toEqual(expected.timetable);
-      
-    });
+  });
     
+    describe("GET /schedule Timetable page view render test", () => {
+    it("Should return schedule Timetable Page", async () => {
+        const req = {
+            query: {},
+        session: {
+          user_id: "test1@gmail.com",
+          type: "staff",
+        },
+      };
+        res.locals = {
+            channel: undefined,
+        };
+      
+
+      await channelController.getschedulel(req, res);
+      expect(res.render).toHaveBeenCalledWith("staff/schedule");
+    });
+    it("Should redirect to loginPage if usertype isnt staff fails", async () => {
+        const req = {
+            query: {},
+        session: {
+          user_id: null,
+          type: null,
+        },
+      };
+        res.locals = {
+            channel: undefined,
+        };
+      
+
+      await channelController.getschedulel(req, res);
+      expect(res.redirect).toHaveBeenCalledWith("/login");
+    });
   });
 
-  describe("/channel getschedulel",  () => {
+    describe("/POST schedule Functionality test", () => {
+    let req;
+    beforeEach(() => {
+      req = {
+        body: {
+          programme_id: "0",
+          day: "0",
+          time: "0",
+        },
+        query: {},
+        session: {
+          user_id: "test1@gmail.com",
+          type: "staff",
+        },
+      };
+    });
 
-    // beforeAll(async ()=>{
-    //   await pool.query(
-    //     `INSERT INTO user (first_name,last_name,type,pet,color,email, password) VALUES (?,?,?,?,?,?,?);`,
-    //       [
-    //         'fname',
-    //         'lname',
-    //         "staff",
-    //         'dog',
-    //         'black',
-    //         'staff@gmail.com',
-    //         'PWD123'
-    //       ]
-    //     );
-    //     await pool.query(
-    //       `INSERT INTO staff(user_id, channeld_id) VALUES (?,?);`,['staff@gmail.com','channel_id'],(err,result)=>{
-    //         console.log(result,err);
-    //       }
-    //     );
-    //     await pool.query(
-    //       'INSERT INTO `channel`(`channeld_id`, `channel_name`) VALUES (?,?)',['channel_id','channel_name']
-    //     );
-    //     console.log(await pool.query('SELECT * FROM channel WHERE channel_id=(select channeld_id from staff where user_id=staff@gmail.com);'))
-    // });
-    beforeEach(async () => {
-       
-        req = {
-           
-            query: {},
-            session: {
-              user_id: 'staff@gmail.com',
-              type: null,
-            },
-            
-          };
+    
+    it("Should redirect to schedule if user type is staff", async () => {
+      await channelController.schedulel(req, res);
+      expect(res.redirect).toHaveBeenCalledWith("schedule");
+    });
+  });
 
-      });
+   describe("/POST add feedback Functionality test", () => {
+    let req;
+    beforeEach(() => {
+      req = {
+        body: {
+          addfeedback: "good program",
+          proid: "0",
+          fav:"fav",
+        },
+        url:"/user/addfeedback/test1",
+        query: {},
+        session: {
+          user_id: "testrecover@gmail.com",
+          type: "user",
+        },
+      };
+    });
 
-      afterAll(async () => {
-    //     await pool.query("DELETE FROM user WHERE email=?;", ["staff@gmail.com"]);
-    //     await pool.query("DELETE FROM staff WHERE channel_id=?;", ["channel_id"]);
-    //     await pool.query("DELETE FROM channel WHERE channel_id=?;", ["channel_id"]);
-    // 
+    
+    it("Should redirect to addfeedback if user type is user", async () => {
+      await channelController.addfeedback(req, res);
+      expect(res.redirect).toHaveBeenCalledWith("/user/" + req.url);
+    });
    });
-
-    it("should return login to login ", async () => {
-    
-      await channel.getschedulel(req, res);
-      expect(res.redirect).toHaveBeenCalledWith(`/login`);
-      
+  
+   describe("/POST add program Functionality test", () => {
+    let req;
+    beforeEach(() => {
+      req = {
+        url:"/user/addfeedback/test1",
+        query: {},
+        session: {
+           user_id: "test1@gmail.com",
+          type: "staff",
+        },
+      };
     });
 
-
-    // it("should return staff/schedule ", async () => {
-    //   req.session.user_id='staff@gmail.com';
-    //   req.session.type='staff';
-    //   await channel.getschedulel(req, res);
-    //   expect(res.render).toHaveBeenCalledWith(`staff/schedule`);
-     
-    // });
     
+    it("Should redirect to schedule if user type is staff", async () => {
+      await channelController.AddProgram(req, res);
+      expect(res.redirect).toHaveBeenCalledWith("schedule");
+    });
+   });
+  
+   describe("GET /addfeedback addfeedback page view render test", () => {
+    it("Should return addfeedback Page", async () => {
+        const req = {
+            query: {},
+        session: {
+          user_id: "test1@gmail.com",
+          type: "staff",
+          },
+          url: "/user/addfeedback/test1",
+        params: {
+                id:"test1"
+            },
+      };
+        res.locals = {
+            channel: undefined,
+        };
+      
+
+      await channelController.get_program(req, res);
+      expect(res.render).toHaveBeenCalledWith("user/addfeedback");
+    });
+    it("Should redirect to loginPage if usertype isnt user", async () => {
+        const req = {
+            query: {},
+        session: {
+          user_id: null,
+          type: null,
+        },
+      };
+        res.locals = {
+            channel: undefined,
+        };
+      
+
+      await channelController.get_program(req, res);
+      expect(res.redirect).toHaveBeenCalledWith("/login");
+    });
   });
-  
-  
 
-  
-
+ 
 });
